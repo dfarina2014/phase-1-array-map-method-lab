8b7c053bd1e6d5c849189ace3dd14b20574412b8
daren farina <daren.farina@gmail.com>
2022-08-31 12:19:30 -0400

    commit tree d82664e2caddfb13cc65819d524c675ebbe6029c

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() { 
+  return tutorials.map(tutorial => (tutorial.split(" ").map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1 )) 
+  }).join(" ")));
+} 
+
